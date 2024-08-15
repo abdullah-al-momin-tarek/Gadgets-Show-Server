e58a9d683c18f0f22ef7a86d94b6e2374fd78664
Shikder Tarek <shikdertarek2019@gmail.com>
2024-08-15 14:38:49 +0600

    Add basic get function

--- index.js
+++ index.js
@@ -27,13 +27,26 @@ const gadgetsCollection = database.collection("gadgetsCollection")
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    app.get("/gadgets", async (req,res)=>{
+
+
+      const result = await gadgetsCollection.find().toArray()
+      console.log(result);
+      res.send(result)
+      
+    })
+
+
+
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
@@ -41,7 +54,7 @@ run().catch(console.dir);
 
 
 app.get("/", (req, res) => {
-    res.send("coffee server is running");
+    res.send("Data server is running");
   });
   app.listen(port, () => {
     console.log("Server running on port: ", port);
