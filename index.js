4390c045885675d60f4a72113257aef156c86396
Shikder Tarek <shikdertarek2019@gmail.com>
2024-08-16 22:56:51 +0600

    Deploy related work

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 // middleware
-app.use(cors())
 app.use(express.json())
 
 const uri = process.env.DB_URI
@@ -28,7 +27,7 @@ const gadgetsTestCollection = database.collection("gadgetsTestCollection")
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     app.get("/gadgets", async (req,res)=>{
       try{
@@ -119,7 +118,7 @@ async function run() {
 
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
